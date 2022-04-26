942fed6b6d35815320cc904c3bca5d716e452403
Manish Kumar <mkdestiny2410@gmail.com>
2022-04-26 15:12:09 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,20 @@
+// JAVA BASIC
+// Average Number
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+		Scanner sc=new Scanner(System.in);
+        String name =sc.next();
+        int m1=sc.nextInt();
+        int m2=sc.nextInt();
+        int m3=sc.nextInt();
+        
+        System.out.println(name);
+        System.out.println((m1+m2+m3)/3);
+		
+	}
+}
+
