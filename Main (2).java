84aafc77365301608261123aa3059e64e041ac6a
Manish Kumar <mkdestiny2410@gmail.com>
2022-04-26 15:10:09 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,19 @@
+// JAVA BASIC
+// Multiple input In one line
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    String [] input;
+	    String a,b;
+	    Scanner sc = new Scanner(System.in);
+	    input=sc.nextLine().split(" ");
+	    a=input[0];
+	    b=input[1];
+	    System.out.println(a+b);
+	   // System.out.println(b);
+	    
+	    
+		
+	}
+}
