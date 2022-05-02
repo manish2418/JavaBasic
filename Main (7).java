1bad57c1a3d07a67a9c4b858a16304fe139a33ca
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-02 22:10:01 +0530

    Add files via upload

---
+++ Main (7).java
@@ -0,0 +1,10 @@
+//Java program to display array using for loop
+public class Main
+{
+	public static void main(String[] args) {
+	    int a[]={10,20,30,40,50};
+	    for(int i=0;i< a.length;i++)
+		System.out.println(a[i]
+		);
+	}
+}
