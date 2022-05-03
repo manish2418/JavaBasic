4be802753e3a6a13b6931031861ea7187e584420
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-03 11:19:00 +0530

    Add files via upload

---
+++ Main (9).java
@@ -0,0 +1,12 @@
+//Java program to calculate the sum of array elements
+public class Main
+{
+	public static void main(String[] args) {
+	    int arr[]={10,20,30};
+	    int sum=0;
+	    for(int i=0;i<arr.length;i++){
+	         sum+=arr[i];
+	    }
+		System.out.println(sum);
+	}
+}
