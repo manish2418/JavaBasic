2c7b9068f17297d2e1518ad532ef07013402567d
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-04 22:52:03 +0530

    Add files via upload

---
+++ Main (18).java
@@ -0,0 +1,22 @@
+//Sorting Arrays
+
+import java.util.Arrays;
+
+public class Main {
+  
+  // main method
+  public static void main(String[] args) {
+
+    // declare and initialize arrays
+    int arr[] = {50, 25, 30, 55, 15};
+    
+    // display array before sorting
+    System.out.println("Before Sorting: " + Arrays.toString(arr));
+    
+    // sort array
+    Arrays.sort(arr);
+    
+    // display array after sorting
+    System.out.println("After Sorting: " + Arrays.toString(arr));
+  }
+}
